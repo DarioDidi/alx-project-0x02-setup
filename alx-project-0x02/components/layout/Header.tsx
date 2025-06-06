57d81132debebf943217f1609f0e31df666ed942
Dario Didi <dididario.dd2@gmail.com>
2025-06-06 17:38:52 +0300

    add links to header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -3,9 +3,12 @@ import Link from "next/link"
 const Header: React.FC = () => {
 	return (
 		<header>
-			<div className="w-full bg-gray-400 shadow-md">
-				<Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+			<div className=" container flex w-full bg-gray-400 shadow-md">
+				<Link href="/home" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
 					Home
+				</Link>
+				<Link href="/about" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+					About
 				</Link>
 			</div>
 		</header>
